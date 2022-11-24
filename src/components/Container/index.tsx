fcda65d266658e25b4e40c547ba0911c9a58be32
victormedeiros1 <josevqczt@gmail.com>
2022-11-24 20:36:29 -0300

    feat: component Container

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ContainerStyles } from './styles';
+
+interface Props {
+  children: JSX.Element;
+}
+
+const Container: React.FC<Props> = ({ children }) => {
+  return <ContainerStyles>{children}</ContainerStyles>;
+};
+
+export default Container;
