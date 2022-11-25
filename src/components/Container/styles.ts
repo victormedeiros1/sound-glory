3483ed5309dc56916f21b4779f54b581bd49e7c2
victormedeiros1 <josevqczt@gmail.com>
2022-11-25 11:41:04 -0300

    feat: background in Container

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -3,10 +3,7 @@ import styled from 'styled-components';
 export const ContainerStyles = styled.div`
   width: 100%;
   min-height: 100vh;
+  max-width: 1920px;
   position: relative;
-
-  background-image: url('/src/assets/images/bg-headphone.jpg');
-  background-size: cover;
-  background-position: -50%;
-  background-repeat: no-repeat;
+  margin: 0 auto;
 `;
