d2f221dddc830c12fd656f6bcfe1a4435e9bd8e1
victormedeiros1 <josevqczt@gmail.com>
2022-11-25 14:44:58 -0300

    feat: component Divider

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,7 @@
+import { DividerStyles } from './styles';
+
+const Divider: React.FC = () => {
+  return <DividerStyles />;
+};
+
+export default Divider;
---
+++ src/components/Divider/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const DividerStyles = styled.div`
+  width: 100%;
+  height: 0.0625rem;
+  background-color: var(--lightGray);
+`;
