f072f8e75cc95dd947de6ddd497f3039fee23c1b
José Victor <josevictordev@gmail.com>
2022-12-28 15:17:04 -0300

    fix: new path for logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,7 +1,8 @@
-import { LogoStyles } from './styles';
+import { LogoStyles } from "./styles";
+import logo from "/src/assets/images/logo.svg";
 
 const Logo: React.FC = () => {
-  return <LogoStyles src="/src/assets/images/logo.svg" />;
+  return <LogoStyles src={logo} />;
 };
 
 export default Logo;
