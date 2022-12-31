0482481daa72f9d4da0e7e1b4e5abcc539813543
José Victor <josevictordev@gmail.com>
2022-12-31 17:32:14 -0300

    chore: re-organize Types

--- src/store/types/playlist.ts
+++ src/store/types/playlist.ts
@@ -1,32 +1,7 @@
-import { Action } from "redux";
-
-export interface ISong {
-  id: string;
-  title: string;
-  description: string;
-  path: string;
-}
-
-interface Song {
-  title: string;
-  description: string;
-  path: string;
-}
+import { Song } from "./song";
 
 export interface Playlist {
   id: string;
   name: string;
   songs?: Song[];
-}
-
-export interface IPLaylistTypes {
-  ADD_PLAYLIST: string;
-}
-
-export interface AddPlaylistAction extends Action<string> {
-  title: string;
-}
-
-export interface IPlaylistCreators {
-  addPlaylist: (name: string) => AddPlaylistAction;
 }
---
+++ src/store/types/song.ts
@@ -0,0 +1,7 @@
+export interface Song {
+  id: number;
+  title: string;
+  description: string;
+  path: string;
+  isPlaying: boolean;
+}
