140ba3b9751c23ef1bf50e306949026d02570bf7
José Victor <josevictordev@gmail.com>
2023-01-10 16:02:41 -0300

    refactor: Dropdown radix css to styled-components

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,7 +1,14 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { CaretRight, DotsThreeVertical } from "phosphor-react";
-import "./styles.css";
 import { Playlist } from "../../store/types/playlist";
+import {
+  DropdownMenuArrow,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  RightSlot,
+} from "./styles";
 
 export interface Props {
   playlists: Playlist[];
@@ -11,39 +18,30 @@ const Dropdown: React.FC<Props> = ({ playlists }) => {
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
-        <button className="IconButton" aria-label="Customise options">
+        <button aria-label="Customise options">
           <DotsThreeVertical size={24} color="#a3a3a3" weight="light" />
         </button>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="dropdown-menu-content" sideOffset={5}>
+        <DropdownMenuContent sideOffset={5}>
           <DropdownMenu.Sub>
-            <DropdownMenu.SubTrigger className="dropdown-menu-sub-trigger">
+            <DropdownMenuSubTrigger>
               Add to playlist
-              <div className="right-slot">
+              <RightSlot>
                 <CaretRight size={14} />
-              </div>
-            </DropdownMenu.SubTrigger>
+              </RightSlot>
+            </DropdownMenuSubTrigger>
             <DropdownMenu.Portal>
-              <DropdownMenu.SubContent
-                className="dropdown-menu-sub-content"
-                sideOffset={8}
-                alignOffset={-8}
-              >
+              <DropdownMenuSubContent sideOffset={8} alignOffset={-8}>
                 {playlists.map(({ id, name }) => (
-                  <DropdownMenu.Item className="dropdown-menu-item" key={id}>
-                    {name}
-                  </DropdownMenu.Item>
+                  <DropdownMenuItem key={id}>{name}</DropdownMenuItem>
                 ))}
-              </DropdownMenu.SubContent>
+              </DropdownMenuSubContent>
             </DropdownMenu.Portal>
           </DropdownMenu.Sub>
-
-          <DropdownMenu.Separator className="dropdown-menu-separator" />
-
-          <DropdownMenu.Arrow className="dropdown-menu-arrow" />
-        </DropdownMenu.Content>
+          <DropdownMenuArrow />
+        </DropdownMenuContent>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
   );
