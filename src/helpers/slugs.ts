fd0655dafcf904f286c30c5d4d2d8fe8a333fe0d
José Victor <josevictordev@gmail.com>
2023-02-28 02:05:45 -0300

    feat: two new songs

---
+++ src/helpers/slugs.ts
@@ -0,0 +1,44 @@
+import { Song } from "../types/song";
+import song1 from "/src/assets/audios/Nossa Toca - Não Temos Tempo.mp3";
+import song2 from "/src/assets/audios/Hungria Hip Hop - Super Herói.mp3";
+import song3 from "/src/assets/audios/Hayya Hayya (Better Together) - FIFA World Cup 2022.mp3";
+import song4 from "/src/assets/audios/Lowz - Libertina.mp3";
+import song5 from "/src/assets/audios/Dani Black - A Vida É Cheia Dessas Coisas.mp3";
+
+export const songs: Song[] = [
+  {
+    id: 0,
+    title: "Não Temos Tempo",
+    description: "Nossa Toca",
+    audio: new Audio(song1),
+    isPlaying: false,
+  },
+  {
+    id: 1,
+    title: "Super Herói",
+    description: "Hungria Hip Hop",
+    audio: new Audio(song2),
+    isPlaying: false,
+  },
+  {
+    id: 2,
+    title: "Hayya Hayya (Better Together) - FIFA World Cup 2022",
+    description: "Trindad Cordona, Davido, Aisha, FIFA Sound",
+    audio: new Audio(song3),
+    isPlaying: false,
+  },
+  {
+    id: 3,
+    title: "Libertina",
+    description: "Louz",
+    audio: new Audio(song4),
+    isPlaying: false,
+  },
+  {
+    id: 4,
+    title: "A vida é cheia dessas coisas",
+    description: "Dani Black",
+    audio: new Audio(song5),
+    isPlaying: false,
+  },
+];
